7eaff81cae26e52f37fbc54b29af5819e31f0207
Brandy Schroeder <brandyschroeder510@gmail.com>
2024-03-01 20:34:08 -0600

    useState for Loading... message

--- src/App.js
+++ src/App.js
@@ -11,14 +11,18 @@ const App = () => {
   // Create additional state to hold user input for limit and start properties
   const [input, updateInput] = useState({ limit: 5, start: 0 });
 
-// Create a new function to allow users to update the input values
+  // Create a variable for loading
+  const [loading, updateLoading] = useState(true);
+
+  // Create a new function to allow users to update the input values
   function updateInputValues(type, value) {
   updateInput({ ...input, [type]: value });
 }
 
   // Define function to all API
   const fetchCoins = async() => {
-    const { limit, start } = input
+    updateLoading(true);
+    const { limit, start } = input;
     //Get request with latest Amplify
     const restOperation = await get({
     apiName: "cryptoapi",
@@ -35,6 +39,7 @@ const App = () => {
     const { body } = await restOperation.response;
     const json = await body.json();
     updateCoins(json.coins);
+    updateLoading(false);
   }
 
   // Call fetchCoins function when component loads
@@ -53,8 +58,9 @@ const App = () => {
         placeholder="limit"
       />
       <button onClick={fetchCoins}>Fetch Coins</button>
+      {loading && <h2>Loading...</h2>}
       {
-        coins.map((coin, index) => (
+        !loading && coins.map((coin, index) => (
           <div key={index}>
             <h2>{coin.name} - {coin.symbol}</h2>
             <h5>${coin.price_usd}</h5>
